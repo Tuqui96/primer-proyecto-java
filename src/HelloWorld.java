f7767c052c8e416b872741e93e3d7b3002f6551e
martaGómez <mgp-96@hotmail.com>
2023-03-23 20:52:02 +0100

    Modificación en rama develop. martaGómez

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
+		System.out.println("Hola M");
+		System.out.println("Hola ");
+		System.out.println("H");
 	}
 
 }
